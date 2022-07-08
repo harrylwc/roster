c5c9d631bb572b3e11c8a6edd14ec21b4c0bf4e2
Wing Chui Lau <wingchuilau@WingdeMac-mini.lan>
2022-07-09 04:31:51 +0800

    modified:   8311.ics 	modified:   csv2ical.py 	modified:   google_cal_08311.ics

--- csv2ical.py
+++ csv2ical.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import csv
-from icalendar import Calendar, Event
+from icalendar import Calendar, Event, Alarm
 from datetime import datetime
 import argparse
 from datetime import timedelta
@@ -8,6 +8,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('input', type=str, help='Input csv file containing calendar events')
 parser.add_argument('output', type=str, help='Output ics file')
 args = parser.parse_args()
+
 
 
 def csv2ical(input_file, output_file):
@@ -25,7 +26,11 @@ def csv2ical(input_file, output_file):
   -------
   Empty
   """
-
+ 
+  ### set reminder before shift start ####
+  tigger_before_shift = timedelta(minutes=-int(60)) 
+  duration = timedelta(minutes=-int(45)) 
+  
   with open(input_file) as csv_file:
     reader = csv.reader(csv_file)
 
@@ -47,22 +52,29 @@ def csv2ical(input_file, output_file):
       summary = row[0]
       if row[2] == '':
            dtstart = datetime.strptime(row[1]+' 00:00', '%Y/%m/%d %H:%M')
+           set_alarm = False 
       else: 
            dtstart = datetime.strptime(row[1]+' '+row[2], '%Y/%m/%d %H:%M')
-      print(dtstart)
+           set_alarm = True 
       dtend = dtstart + timedelta(minutes=495) 
-      print(dtend) 
       description = row[5].strip()
       location = row[4].strip()
-
+     
       event = Event()
       event.add('summary', summary)
       event.add('dtstart', dtstart)
       event.add('dtend', dtend)
       event.add('description', description)
       event.add('location', location)
+      if set_alarm == True :
+         alarm=Alarm()
+         alarm.add('TRIGGER',tigger_before_shift)
+         alarm.add('DESCRIPTION','返工啦')
+         alarm.add('REPEAT','3')
+         alarm.add('DURATION',duration)
+         alarm.add('ACTION','DISPLAY')
+         event.add_component(alarm)
       cal.add_component(event)
-
     with open(output_file, 'wb') as out_f:
       out_f.write(cal.to_ical())
       out_f.close()
